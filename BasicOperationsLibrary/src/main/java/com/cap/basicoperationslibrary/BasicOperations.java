6c65cde4b4127141844662e4acbdf9b5dd5ebeab
NAGARAJ <keerthana.nagaraj@capgemini.com>
2024-08-20 19:44:16 +0530

    Initial commit

---
+++ BasicOperationsLibrary/src/main/java/com/cap/basicoperationslibrary/BasicOperations.java
@@ -0,0 +1,20 @@
+package com.cap.basicoperationslibrary;
+
+
+public class BasicOperations {
+    public int addNumbers(int a, int b){
+        return a+b;
+    }
+    public int subtractNumbers(int a, int b){
+        return a-b;
+    }
+    public int multiplyNumbers(int a, int b){
+        return a*b;
+    }
+    public int divideNumbers(int a, int b){
+        return a/b;
+    }
+    public int moduloNumbers(int a, int b){
+        return a%b;
+    }
+}
---
+++ app/src/main/java/com/cap/basicoperationsapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.cap.basicoperationsapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.cap.basicoperationslibrary.BasicOperations;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText number1,number2;
+    private Button btAdd,btSub,btMultiply,btDivide;
+    private TextView result;
+
+    BasicOperations basicOperations = new BasicOperations();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        number1 = findViewById(R.id.tv1);
+        number2 = findViewById(R.id.tv2);
+
+        btAdd = findViewById(R.id.btnAdd);
+        btSub = findViewById(R.id.btnSubtract);
+        btMultiply = findViewById(R.id.btnMultiply);
+        btDivide = findViewById(R.id.btnDivide);
+
+        result = findViewById(R.id.answer);
+
+        btAdd.setOnClickListener(view -> {
+            int a = Integer.parseInt(String.valueOf(number1.getText()));
+            int b = Integer.parseInt(String.valueOf(number2.getText()));
+            int sum = basicOperations.addNumbers(a,b);
+            result.setText(String.format(
+                    getResources().getString(R.string.reesult_is_d),sum));
+        });
+
+        btSub.setOnClickListener(view -> {
+            int a = Integer.parseInt(String.valueOf(number1.getText()));
+            int b = Integer.parseInt(String.valueOf(number2.getText()));
+            int sub = basicOperations.subtractNumbers(a,b);
+            result.setText(String.format(
+                    getResources().getString(R.string.reesult_is_d),sub));
+        });
+
+        btMultiply.setOnClickListener(view -> {
+            int a = Integer.parseInt(String.valueOf(number1.getText()));
+            int b = Integer.parseInt(String.valueOf(number2.getText()));
+            int multiply = basicOperations.multiplyNumbers(a,b);
+            result.setText(String.format(
+                    getResources().getString(R.string.reesult_is_d),multiply));
+        });
+
+        btDivide.setOnClickListener(view -> {
+            int a = Integer.parseInt(String.valueOf(number1.getText()));
+            int b = Integer.parseInt(String.valueOf(number2.getText()));
+            int divide = basicOperations.divideNumbers(a,b);
+            result.setText(String.format(
+                    getResources().getString(R.string.reesult_is_d),divide));
+        });
+
+
+
+    }
+}
